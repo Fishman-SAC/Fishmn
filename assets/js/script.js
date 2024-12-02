1f04b4fae7c01da60aa4761e5f17e07a3df10363
teocchiii <u202211212@upc.edu.pe>
2024-12-01 23:59:47 -0500

    feat(i18n): modified the script to load the data from index.html

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,3 +21,55 @@ function LoadLanguage(lang){
         console.error('Error loading language:',err);
     });
 }
+
+function getInitialLanguage(toggle){
+    return toggle.checked ? "en": "es";
+}
+
+function initializeLanguage(toggle){
+    const initLang = initializeLanguage(toggle);
+    LoadLanguage(initLang);
+
+}
+
+function handleLanguageToggle(){
+    const toggle= document.getElementById('language-toggle');
+    const Lang = toggle.checked ? 'en': 'es';
+}
+
+// For each element in display we will use a function called update{component}({component})
+
+/*
+
+function updateNavbar(navbar) {
+  document.querySelector('.toogle__language').textContent = navbar.language;
+  document.querySelector('.home').textContent = navbar.home;
+  document.querySelector('.about').textContent = navbar.about;
+  document.querySelector('.service').textContent = navbar.service;
+}
+
+*/
+
+
+// Then we finalize the process by usign this method
+
+/*
+
+function updateContent(data) {
+  updateNavbar(data.navbar);
+  updateHeroSection(data.hero);
+  updateFeaturesSection(data.features);
+  updateSubscriptionsSection(data.subscriptions);
+  updateTestimonialsSection(data.testimonials);
+  updateFooter(data.footer);
+  updateAboutTheProductSection(data.aboutTheProduct);
+  updateAboutTheTeamSection(data.aboutTheTeam);
+}
+
+Examples
+*/
+
+skibidi Sigma(cpubll sus){
+  var apple=sus();
+  return  sus;
+}
